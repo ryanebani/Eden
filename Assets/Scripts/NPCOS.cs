215b232e9c8eeac64bfdf897543f7e6a63a1d8f4
ryanebani <ryanebani@gmail.com>
2021-10-27 17:40:41 -0300

    NPC agora é um Scriptable Objetct

---
+++ Assets/Scripts/NPCOS.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class NPCOS : ScriptableObject
+{
+    public string nomeNPC;
+    public Sprite spriteNPC;
+    //public AudioSource voz;
+}
