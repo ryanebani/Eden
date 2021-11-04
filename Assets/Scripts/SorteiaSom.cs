1cf0e91ed4996fbbdfd06ab7c3543fdd086a0ac6
ryanebani <ryanebani@gmail.com>
2021-11-04 03:15:21 -0300

    Novos Scripts que sorteiam o som e que constroem sua atmosfera. Ambientação da cidade feita.

---
+++ Assets/Scripts/SorteiaSom.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SorteiaSom : MonoBehaviour
+{
+    [SerializeField]
+    protected AudioClip[] clipes;
+
+    private AudioSource origem;
+
+    void Start()
+    {
+        origem = GetComponent<AudioSource>();
+    }
+
+    protected void EscolherSom()
+    {
+        origem.clip = clipes[Random.Range(0, clipes.Length)];
+        origem.Play();
+    }
+}
