6b8e2314b136c90cb54a075c7ca036f02e05fcaa
Rafael <r4fa38@gmail.com>
2021-11-22 21:36:38 -0300

    Hud ajustada

---
+++ Assets/BotaoMask.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BotaoMask : MonoBehaviour
+{
+    [SerializeField]Image filho;
+  
+    void Update()
+    {
+        if(Dialogo.mascara == true)
+        {
+            filho.enabled = true;
+        }else
+        {
+            filho.enabled = false;
+        }
+
+    }
+}
