d5de150fb1df34176861d16ec9955c9e3f099953
ryanebani <ryanebani@gmail.com>
2022-05-16 01:22:14 -0300

    Créditos atualizados, agora tá responsa

--- Assets/Creditos.cs
+++ Assets/Creditos.cs
@@ -1,20 +1,47 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Creditos : MonoBehaviour
 {
-    [SerializeField] float velocidade;
+    [SerializeField] float tempoDeTela;
+    [SerializeField] float valorVel;
+    float velocidade;
+
     Transform transformObjeto;
+
+    Vector3 posInicial;
 
     void Start()
     {
         transformObjeto = GetComponent<Transform>();
+        posInicial = GetComponent<Transform>().position;
     }
 
     void Update()
     {
         transformObjeto.Translate(Vector3.up * velocidade * Time.deltaTime);
     }
-  
+
+    public void SetCreditos()
+    {
+        velocidade = 0;
+        transformObjeto.position = posInicial;
+        StartCoroutine(ContagemAntes());
+    }
+
+    IEnumerator ContagemAntes()
+    {
+        yield return new WaitForSeconds(tempoDeTela);
+        velocidade = valorVel;
+    }
+
+    public void Acelerar()
+    {
+        if (velocidade == valorVel)
+        {
+            velocidade *= 3;
+        }
+        else
+            velocidade = valorVel;
+    }
 }
